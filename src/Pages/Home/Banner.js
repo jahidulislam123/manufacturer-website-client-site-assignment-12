5e2d13e1ad528fccd04ad4e6a20f35f1e6098572
unknown <islam15-3879@diu.edu.bd>
2022-05-22 09:46:58 +0600

    navbar-and-carusel-adde

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+       
+            <div>
+                <div class="carousel carousel-end rounded-box">
+  <div class="carousel-item">
+    <img src={'https://i.ibb.co/jMZyCX0/istockphoto-516240596-170667a.jpg'} alt="Drink" />
+  </div> 
+  <div class="carousel-item">
+    <img src={'https://i.ibb.co/zXgh4mS/istockphoto-1140515858-170667a.jpg'} alt="Drink" />
+  </div> 
+  <div class="carousel-item">
+    <img src={'https://i.ibb.co/Jd9RcLf/istockphoto-1059130086-170667a.jpg'} alt="Drink" />
+  </div> 
+  <div class="carousel-item">
+    <img src={'https://i.ibb.co/zrLVFNj/fxf5km.jpg'} alt="Drink" />
+  </div> 
+  <div class="carousel-item">
+    <img src={'https://i.ibb.co/mGwhcmc/istockphoto-615281154-170667a.jpg'} alt="Drink" />
+  </div> 
+  <div class="carousel-item">
+    <img src={'https://i.ibb.co/hFbV1WT/t0cjg6.jpg'} alt="Drink" />
+  </div> 
+  
+</div>
+            </div>
+       
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2>this is home page</h2>
         </div>
     );
