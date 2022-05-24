cc227b0e1850dc030194327e1aa3c6aabb31bed2
unknown <islam15-3879@diu.edu.bd>
2022-05-25 02:22:39 +0600

    added

--- src/Hooks/useToken.js
+++ src/Hooks/useToken.js
@@ -6,7 +6,7 @@ const useToken = user =>{
    const email =user?.user?.email;
    const currentUser ={email:email};
    if(email){
-       fetch(` https://radiant-temple-88405.herokuapp.com/user/${email}`,{
+       fetch(`https://radiant-temple-88405.herokuapp.com/user/${email}`,{
            method:'PUT',
            headers :{
                 'content-type':'application/json'
